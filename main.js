4fd6ade1d5b580008dcceaf9eca79992decafa7a
ArgentumSunset <coltergiem@gmail.com>
2015-11-17 18:33:43 -0700

    Tinkering

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 var univ = $('#universe').width();
 var univContent = "<img src='imgs/sun.png' class='body' id='sun'>"
-var realUniv = 1.75; // Distances calculated in AU
-var data = [["mercury", 2440, .387, 87.969, "imgs/mercury.png"],
+var realUniv = 1.5; // Distances calculated in AU
+var innerPlanets = [["mercury", 2440, .387, 87.969, "imgs/mercury.png"],
 ["venus", 6052, .730, 224.701, "imgs/venus.png"],
 ["earth", 6378, 1, 365.256, "imgs/earth.png"],
 ["mars", 3397, 1.524, 686.971, "imgs/mars.png"]];
-var planets = [];
+var outerPlanets = []
+var planets = []
 var rate = 2; // days / sec
 var refreshRate = 100
 
 $(document).ready(function () {
-    for (var i = 0; i < data.length; i++) {
-        planet = new Planet(data[i]);
-        drawOrbit(planet);
-        drawPlanet(planet);
-        planets[i] = planet;
-        $('#universe').html(univContent);
-    }
+    getData(innerPlanets);
     setInterval(function () {
         for (var i = 0; i < planets.length; i++) {
             movePlanet(planets[i])
         }
     }, 100);
+
+    $('#sun').click(function () {
+        changeSize(3)
+    });
 
 })
 
@@ -32,7 +31,7 @@ function drawOrbit(planet) {
 
 function drawPlanet(planet) {
     curPlanet = "<img class='body planet' id='" + planet.name + "' src='" + planet.img + "' style='height:" + planet.radius + "px;width:" + planet.radius + "px;top:" + ((univ / 2 + planet.orbitRadius) - planet.radius / 2) + "px;left:" + (univ / 2 - planet.radius / 2) + "px;'>"
-    curPlanet += "<h1 class='planet-name' id='" + planet.name + "-name' style='px;top:" + ((univ / 2 + planet.orbitRadius - 20) - planet.radius / 2) + "px;left:" + (univ / 2 - planet.radius / 2 + 30) + "px;'>" + capitalizeFirstLetter(planet.name) + "</h1>"
+    curPlanet += "<h3 class='planet-name' id='" + planet.name + "-name' style='px;top:" + ((univ / 2 + planet.orbitRadius - 20) - planet.radius / 2) + "px;left:" + (univ / 2 - planet.radius / 2 + 30) + "px;'>" + capitalizeFirstLetter(planet.name) + "</h3>"
     univContent += curPlanet
 }
 
@@ -53,4 +52,25 @@ function movePlanet(planet) {
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getData(data) {
+    for (var i = 0; i < data.length; i++) {
+        planet = new Planet(data[i]);
+        drawOrbit(planet);
+        drawPlanet(planet);
+        planets[i] = planet;
+        $('#universe').html(univContent);
+    }
+}
+
+function changeSize(setting) {
+    $('#universe').fadeOut(300);
+    setTimeout(function () {
+        univContent = "<img src='imgs/sun.png' class='body' id='sun'>";
+        planets = [];
+        realUniv = setting;
+        getData(innerPlanets);
+    }, 300);
+    $('#universe').fadeIn(300);
 }
--- planet.js
+++ planet.js
@@ -1,6 +1,6 @@
 function Planet(planet) {
     this.name = planet[0];
-    this.radius = (univ / 18) * (planet[1] / 6378);
+    this.radius = (univ / (15 * realUniv)) * (planet[1] / 6378);
     this.orbitDiameter = planet[2] * (univ / realUniv); // Converting between AU and px
     this.orbitRadius = this.orbitDiameter / 2
     this.year = planet[3];
